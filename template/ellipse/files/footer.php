81230961d25652a1b4820e2b3dddfbe9a8c00f37
mknizka <mknizka@gmail.com>
2026-09-24 22:22:02 +0200

    Restore visible closing CTA and accessible SVG article reaction cards

--- template/ellipse/files/footer.php
+++ template/ellipse/files/footer.php
@@ -3,7 +3,7 @@
 <?php if (isset($content['extra_js_footer'])) { echo $content['extra_js_footer']; } ?>
 <?php echo themeSetup('extra_body_end'); ?>
 <script src="/template/ellipse/js/hg-editorial.js?v=3" defer></script>
-<script src="/template/ellipse/js/hg-icons.js?v=1" defer></script>
+<script src="/template/ellipse/js/hg-icons.js?v=2" defer></script>
 <script src="/template/ellipse/js/hg-navigation.js?v=1" defer></script>
 </body>
 </html>
--- template/ellipse/js/hg-icons.js
+++ template/ellipse/js/hg-icons.js
@@ -17,9 +17,24 @@
    }
   });
  }
- clean(document);
+ const paths={
+  like:'M7 10v11H3V10h4Zm0 0 5-8c3 0 2 5 1 7h6a2 2 0 0 1 2 2l-2 8a2 2 0 0 1-2 2H7',
+  love:'M20.8 4.6a5.5 5.5 0 0 0-7.8 0L12 5.7l-1.1-1.1a5.5 5.5 0 0 0-7.8 7.8L12 21l8.8-8.6a5.5 5.5 0 0 0 0-7.8Z',
+  clap:'m5 12 4 4L19 6M3 6l1-2m7-1V1m8 16 2 1M6 21l-2 1',
+  think:'M9 9a3 3 0 1 1 5 2c-2 1-2 2-2 3M12 18h.01M22 12a10 10 0 1 1-20 0 10 10 0 0 1 20 0Z',
+  wow:'m12 2 3 6 7 1-5 5 1 8-6-4-6 4 1-8-5-5 7-1 3-6Z',
+  useful:'M9 18h6M9 21h6M8 14a7 7 0 1 1 8 0l-1 2H9l-1-2Z'
+ };
+ function decorate(){document.querySelectorAll('.article-emotions .emotion-btn').forEach(button=>{
+  const path=paths[button.dataset.emotion];
+  if(!path||button.querySelector('.hg-reaction-svg'))return;
+  const svg=document.createElementNS('http://www.w3.org/2000/svg','svg');
+  svg.setAttribute('viewBox','0 0 24 24');svg.setAttribute('class','hg-reaction-svg');svg.setAttribute('aria-hidden','true');
+  const shape=document.createElementNS('http://www.w3.org/2000/svg','path');shape.setAttribute('d',path);svg.append(shape);button.prepend(svg);
+ });}
+ clean(document);decorate();
  new MutationObserver(records=>records.forEach(record=>{
   if(record.type==='characterData') {const el=record.target.parentElement?.closest(selector);if(el) clean(el);}
-  else {clean(record.target);record.addedNodes.forEach(clean);}
+  else {clean(record.target);record.addedNodes.forEach(clean);decorate();}
  })).observe(document.body,{childList:true,subtree:true,characterData:true});
 })();
